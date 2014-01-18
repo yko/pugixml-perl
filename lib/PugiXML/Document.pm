b63c6812700eae52df90638a0e7188e9eef82017
Yaroslav Korshak <ykorshak@gmail.com>
2014-01-18 21:26:15 +0400

    brief documentation for PugiXML::Node

--- lib/PugiXML/Document.pm
+++ lib/PugiXML/Document.pm
@@ -8,3 +8,47 @@ use PugiXML;
 use parent 'PugiXML::Node';
 
 1;
+
+__END__
+
+=head1 NAME
+
+PugiXML::Document - XML Document representation
+
+
+=head1 SYNOPSIS
+
+    use PugiXML::Document;
+    my $doc = PugiXML::Document->new;
+
+    my $error = $doc->load('<foo>bar</foo>');
+    if ($error) {
+        die "Bad thing jsut happened: $error";
+    }
+
+=head1 DESCRIPTION
+
+TBD
+
+=head1 METHODS
+
+L<PugiXML::Document> implements following methods:
+
+=head2 C<new>
+
+    my $obj = Class->new;
+    my $obj = Class->new(name => 'value');
+    my $obj = Class->new({name => 'value'});
+
+This base class provides a basic object constructor.
+You can pass it either a hash or a hash reference with attribute values.
+
+=head1 SEE ALSO
+
+=over
+
+=item * L<PugiXML::Node>
+
+=item * L<PugiXML::XMLParseResult>
+
+=back
